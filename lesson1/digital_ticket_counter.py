93197628cee34c10c1f6e2f8c74b4d8c834c758b
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-07-16 14:47:48 +0200

    Added lesson challenge: digital_ticket_counter.py

---
+++ lesson1/digital_ticket_counter.py
@@ -0,0 +1,6 @@
+# The Challenge: “The Concert Ticket Booker”
+
+name = input("What's your name? ")
+artist_to_book = input("Who do you want to book a ticket for? ")
+
+print(f"Hello {name} and thanks for booking with us, your tickets to book {artist_to_book} are successful!")
