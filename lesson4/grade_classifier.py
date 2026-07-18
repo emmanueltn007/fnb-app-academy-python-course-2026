148ee70213938b732bae04d35a869dce2346d313
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-07-18 19:24:32 +0200

    Made minor changes

--- lesson4/grade_classifier.py
+++ lesson4/grade_classifier.py
@@ -10,29 +10,38 @@ average_mark = (subject1_mark + subject2_mark + subject3_mark) / 3
 print(f"Student name: {student_name}") 
 
 print("SUBJECT 1:")
+
 print(f"Mark: {subject1_mark}%")
 
 if subject1_mark < 40:
-    print("Status: Fail")
     print("Needs intervention")
 else:
-    print("Status: Pass")
+    print("Intervention: None needed")
 
 print("SUBJECT 2:")
+
+print(f"Mark: {subject1_mark}%")
+
 if subject2_mark < 40:
-    print("Status: Fail")
     print("Needs intervention")
 else:
-    print("Status: Pass")
+    print("Intervention: None needed")
 
 print("SUBJECT 3:")
+
+print(f"Mark: {subject1_mark}%")
+
 if subject3_mark < 40:
-    print("Status: Fail")
-    print("Needs intervention")
+    print("Intervention: Needs intervention")
 else:
-    print("Status: Pass")
+    print("Intervention: None needed")
 
 print(f"Average mark: {round(average_mark)}")
+
+if average_mark < 40:
+    print("FAIL")
+else: 
+    print("PASS")
 
 if average_mark >= 80:
     print("Grade: A")
