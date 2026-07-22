12669d6e2db7b1ea384a4fe05305d7d90728aad5
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-07-22 15:40:17 +0200

    Added lesson 5

---
+++ lesson6/lesson6.py
@@ -0,0 +1,23 @@
+# Working list (Shopping Cart)
+
+# cart = ["Apples", "Eggs", "Milk"]
+
+# print(cart[0])
+
+# cart.append("Bread")
+
+# print(cart)
+
+# for item in cart:
+#     print(item)
+
+# Working with dictionaries
+
+student = {
+    "name": "Thabo",
+    "age": 25,
+    "course": "Intro to Python"
+}
+
+print(student["name"])
+print(student["age"])
